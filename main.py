42c8eaa8aa1c657033294cae9829f6e985948d38
Ezra Citron <ezracitron@gmail.com>
2025-02-21 13:53:39 +0000

    u

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 from flask import Flask, request, jsonify
 import psycopg2
 import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
 
 app = Flask(__name__)
 
 # PostgreSQL configuration from Railway environment variables
 DB_URL = os.getenv("DATABASE_URL")
 
+
 def connect_db():
     return psycopg2.connect(DB_URL)
 
-@app.route('/update_record', methods=['POST'])
-def update_record():
-    data = request.json
-    record_id = data.get('id')
-    new_price = data.get('price')
 
-    if not record_id or not new_price:
+@app.route("/update_record", methods=["POST"])
+def update_record():
+    logging.info("test")
+    data = request.json
+    record_id = data.get("id")
+    # new_price = data.get('action')
+
+    if not record_id:
         return jsonify({"error": "Missing 'id' or 'price'"}), 400
 
     try:
         conn = connect_db()
         cursor = conn.cursor()
         cursor.execute(
-            "UPDATE whiskey_prices SET price = %s WHERE id = %s",
-            (new_price, record_id)
+            "UPDATE whiskey_prices SET action = %s WHERE id = %s", (1, record_id)
         )
         conn.commit()
         cursor.close()
         conn.close()
-        return jsonify({"status": "success", "message": "Record updated successfully"}), 200
+        return (
+            jsonify({"status": "success", "message": "Record updated successfully"}),
+            200,
+        )
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-@app.route('/hello', methods=['GET'])
+
+@app.route("/hello", methods=["GET"])
 def hello_world():
     return jsonify({"message": "Hello, World!"}), 200
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)), debug=True)
